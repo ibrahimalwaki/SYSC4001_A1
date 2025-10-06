81a070d7064f550cb296fad479347e053eb5918f
smaaroof <sahandmaroof@gmail.com>
2025-10-05 23:46:30 -0400

    updated interrupts.cpp and added test cases

--- interrupts.cpp
+++ interrupts.cpp
@@ -1,82 +1,111 @@
 /**
  *
  * @file interrupts.cpp
- * @author <Your Name>
+ * @author Sasisekhar Govind
+ * 
+ * @author Ibrahim Alwaki 101235067
+ * @author Sahand Maaroof 101311395
  *
  */
 
- #include "interrupts.hpp"
+#include "interrupts.hpp"
 
- int main(int argc, char** argv) {
-     // vectors = ISR addresses, delays = device delays
-     auto [vectors, delays] = parse_args(argc, argv);
-     std::ifstream input_file(argv[1]);
- 
-     std::string trace;      //!< string to store single line of trace file
-     std::string execution;  //!< string to accumulate the execution output
-  
-     //added code
-     int current_time = 0;   //!< simulation clock
- 
-     // constants
-     const int KERNEL_SWITCH_MS = 1;
-     int CONTEXT_SAVE_MS = 10;     //vary 10/20/30
-     const int VECTOR_LOOKUP_MS = 1;
-     const int LOAD_ISR_ADDR_MS = 1;
-     const int DRIVER_ACTIVITY_MS = 40; //vary 40..200
-     const int IRET_MS = 1;
- 
-     // helper: append one line
-     auto log = [&](int dur, const std::string& what) {
-         execution += std::to_string(current_time) + ", "
-                    + std::to_string(dur) + ", "
-                    + what + "\n";
-         current_time += dur;
-     };
- 
-     // helper: run an interrupt (boilerplate + driver activities + IRET)
-     auto run_interrupt = [&](int dev, int activities) {
-         auto [pre, after] = intr_boilerplate(current_time, dev, CONTEXT_SAVE_MS, vectors);
-         execution += pre;
-         current_time = after;
- 
-         for (int i = 0; i < activities; i++) {
-             log(DRIVER_ACTIVITY_MS, "call device driver (activity " + std::to_string(i+1) + ")");
-         }
-         log(IRET_MS, "IRET");
-     };
-     //end of the added code 
-  
-     //parse each line of the input trace file
-     while (std::getline(input_file, trace)) {
-         auto [activity, val] = parse_trace(trace);
+int main(int argc, char** argv) {
 
-      // added code
- 
-         if (activity == "CPU") {
-             log(val, "CPU burst");
-         }
-         else if (activity == "SYSCALL") {
-             int dev = val;
-             // system call interrupt (use 3 driver activities)
-             run_interrupt(dev, 3);
-             // after ISR, we assume device I/O is now in progress
-         }
-         else if (activity == "END_IO") {
-             int dev = val;
-             // when I/O finishes, log the interrupt + ISR
-             log(delays[dev], "end of I/O " + std::to_string(dev) + ": interrupt");
-             run_interrupt(dev, 1); // typically 1 activity for completion
-         }
-         else {
-         }
-     }
-     // end of added code 
-     input_file.close();
-     write_output(execution);
-     return 0;
- }
- //command kine to run the code 
- //bash build.sh
- //./bin/interrupts trace.txt vector_table.txt device_table.txt
- //head -20 execution.txt
+    // vectors: ISR addresses; delays: device delays (index = device number)
+    auto [vectors, delays] = parse_args(argc, argv);
+    std::ifstream input_file(argv[1]);
+
+    std::string trace;      //!< string to store single line of trace file
+    std::string execution;  //!< string to accumulate the execution output
+
+    /******************ADD YOUR VARIABLES HERE*************************/
+
+    int current_time = 0;
+    const int context_save_time = 10;   // 10, 20, 30
+    const int isr_activity_time = 40;   // 40-200
+
+    /******************************************************************/
+
+    // helper to append one line and advance time
+    auto log = [&](int dur, const std::string& what) {
+        execution += std::to_string(current_time) + ", " +
+                     std::to_string(dur) + ", " + what + "\n";
+        current_time += dur;
+    };
+
+    //parse each line of the input trace file
+    while (std::getline(input_file, trace)) {
+        auto [activity, duration_intr] = parse_trace(trace);
+
+        if (activity == "CPU") {
+            execution += std::to_string(current_time) + ", " + std::to_string(duration_intr) + ", CPU\n";
+            current_time += duration_intr;
+        }
+
+        else if (activity == "SYSCALL") {
+            auto [partial, time_after] = intr_boilerplate(current_time, duration_intr, context_save_time, vectors);
+            execution += partial;
+            current_time = time_after;
+
+            // run the ISR
+            int delay_time = delays.at(duration_intr);
+            int part1 = delay_time / 2;
+            int part2 = delay_time - part1;
+
+            execution += std::to_string(current_time) + ", " + std::to_string(part1) + ", run device driver (part 1)\n";
+            current_time += part1;
+
+            execution += std::to_string(current_time) + ", " + std::to_string(part2) + ", run device driver (part 2)\n";
+            current_time += part2;
+
+            // return
+            execution += std::to_string(current_time) + ", 1, IRET\n";
+            current_time += 1;
+
+            // switching back to user mode
+            execution += std::to_string(current_time) + ", 1, switch to user mode\n";
+            current_time += 1;
+        }
+        else if (activity == "END_IO") {
+            execution += std::to_string(current_time) + ", 1, switch to kernal mode\n";
+            current_time += 1;
+
+            execution += std::to_string(current_time) + ", " + std::to_string(context_save_time) + ", context save\n";
+            current_time += context_save_time;
+
+            execution += std::to_string(current_time) + ", 1, find vector " + std::to_string(duration_intr) + "\n";
+            current_time += 1;
+
+            execution += std::to_string(current_time) + ", 1, load address " + vectors.at(duration_intr) + " into PC\n";
+            current_time += 1;
+
+            // ISR body
+            execution += std::to_string(current_time) + ", 40, ENDIO: run the ISR\n";
+            current_time += 40;
+
+            execution += std::to_string(current_time) + ", 1, IRET\n";
+            current_time += 1;
+
+            execution += std::to_string(current_time) + ", 1, switch to user mode\n";
+            current_time += 1;
+        }
+        else {
+            std::cerr << "Unknown activity: " << activity << std::endl;
+        }
+    }
+
+    /************************************************************************/
+
+    input_file.close();
+    write_output(execution);
+    return 0;
+}
+
+// running the code (through windows cmd)
+// g++ -g -O0 -I . -o bin\interrupts.exe interrupts.cpp
+// bin\interrupts.exe trace.txt vector_table.txt device_table.txt
+
+// running on WSL/macOS/Linux
+// source build.sh
+// ./bin/interrupts trace.txt vector_table.txt device_table.txt
